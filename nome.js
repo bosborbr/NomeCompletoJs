b24116b23e441c8b596b1688bbacb363feb0cd82
bosborbr <robsonbosbor@gmail.com>
2021-04-21 00:37:10 -0300

    inicio

---
+++ nome.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digite seu primeiro nome: ");
+let sobrenome = prompt("Digite seu sobrenome: ");
+
+alert(`Nome completo: ${nome} ${sobrenome}`);
+alert(`Nome de catálogo: ${sobrenome} ${nome}`);
